86f45bd8e8db4972d04ae5c38aaee8dd02967f3d
Tim Glen <16306246+timmayg@users.noreply.github.com>
2024-02-03 13:19:51 -0500

    commit

---
+++ cat9k-ssh-key.sh
@@ -0,0 +1,8 @@
+currdate=$(date +'%Y-%m-%d_%H-%M-%S')
+echo "Current Date and Time is $currdate" >> ~/.ssh/known_hosts
+echo "Adding Cat9300 SSH Keys for Cisco Live" >> ~/.ssh/known_hosts
+ssh-keyscan 10.1.1.5 >> ~/.ssh/known_hosts
+ssh-keyscan -p 830 10.1.1.5 >> ~/.ssh/known_hosts
+ssh-keyscan 10.1.1.55 >> ~/.ssh/known_hosts
+ssh-keyscan -p 830 10.1.1.55 >> ~/.ssh/known_hosts
+
